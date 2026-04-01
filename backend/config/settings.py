4f848d2dce251f17a8927703d2014120d4c1e7fa
Phong Nguyễn <nguyenphong12a1nh3@gmail.com>
2026-04-01 20:31:24 +0700

    fixing to deploy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kwru!=&upro=gk_=wc#s_nguh-n=8=z0ntpy(%ubq8u7#0%o@s'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kwru!=&upro=gk_=wc#s_nguh-n=8=z0ntpy(%ubq8u7#0%o@s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # <--- Thêm dòng này lên đầu hoặc gần đầu
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,6 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,5 +9,4 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Django server..."
-exec python manage.py runserver 0.0.0.0:8000
-
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
